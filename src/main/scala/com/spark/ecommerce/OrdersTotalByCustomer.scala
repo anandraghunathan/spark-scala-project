c11d688af263ecd33ef862918ac6a37a08d08948
Anand Raghunathan <anand.raghunathan@hotmail.com>
2020-04-20 18:19:28 -0700

    New modules added

---
+++ src/main/scala/com/spark/ecommerce/OrdersTotalByCustomer.scala
@@ -0,0 +1,31 @@
+package com.spark.ecommerce
+
+import org.apache.spark.SparkContext
+import org.apache.log4j._
+
+object OrdersTotalByCustomer {
+
+  def getCustIdAndAmountSpent(line : String) = {
+    val fields = line.split(",")
+    val custId = fields(0).toInt
+    val price = fields(2).toFloat
+
+    (custId, price)
+  }
+  def main(args : Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sc = new SparkContext("local[*]", "OrdersTotalByCustomer")
+
+    val inputLines = sc.textFile("datasets/customer-orders.csv")
+
+    val mappedInput = inputLines.map(getCustIdAndAmountSpent)
+
+    val ordersTotalByCustomer = mappedInput.reduceByKey((x, y) => x + y)
+
+    val results = ordersTotalByCustomer.collect()
+
+    // Sorted by max amount spent by the customer
+    results.sortWith(_._2 > _._2).foreach(println)
+  }
+}
